6174ba1e5932a81639378f26b93e5e8a05f74b7d
akarat <akrs33@gmail.com>
2019-10-30 09:35:23 +0700

    add Akamai user

--- manage/user/user.sql
+++ manage/user/user.sql
@@ -1,2 +1,3 @@
 INSERT INTO storeasservice.users( username,password,active,sitegroup,site,role) VALUES ( 'admin','password',1,'SG_SZ','','manager');
-INSERT INTO storeasservice.users( username,password,active,sitegroup,site,role) VALUES ( 'user','password',1,'SG_SZ','1055','user');
+INSERT INTO storeasservice.users( username,password,active,sitegroup,site,role) VALUES ( '1055','63492',1,'SG_SZ','1055','user');
+INSERT INTO storeasservice.users( username,password,active,sitegroup,site,role) VALUES ( '1025','63252',1,'SG_SZ','1025','user');
